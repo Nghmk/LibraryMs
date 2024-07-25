1fc3bca9a12b474ddacc75de769230660883f30e
NGHMK <madhushikakumari0403@gmail.com>
2024-07-25 20:40:44 +0530

    Revert "Revert "Revert "Revert "Basic things""""
    This reverts commit d0dd40090e4dac3ec13a796939b613308551a81a.

---
+++ src/edu/ijse/libraryMS/dao/DaoFactory.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.ijse.libraryMS.dao;
+
+import edu.ijse.libraryMS.dao.custom.impl.MemberDaoImpl;
+
+/**
+ *
+ * @author heman
+ */
+public class DaoFactory {
+    private static DaoFactory daoFactory;
+
+    
+    private DaoFactory(){}
+    
+    public static DaoFactory getInstance(){
+     if(daoFactory == null){
+          daoFactory = new DaoFactory();     
+     }
+     return daoFactory;
+    }
+    
+    public SuperDao getDao(DaoTypes types){
+        switch (types) {
+            case MEMBERS:
+                return new MemberDaoImpl();
+          
+            default:
+               return null;
+        }
+ 
+    }
+  
+   public enum DaoTypes{
+       MEMBERS, BOOKS, PUBLISHER, GENRES, TRANSACTIONS, FINE;
+  
+   }  
+}
